ce409a882e94a2d8934f3d06939e9ffa10bc6ae7
anilkumar541 <anilec4870@gmail.com>
2024-06-03 17:39:22 +0530

    cors update

--- worklist/settings.py
+++ worklist/settings.py
@@ -124,5 +124,5 @@ CORS_ALLOWED_ORIGINS = [
 
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://effortless-unicorn-fe58c0.netlify.app/"
+    "https://effortless-unicorn-fe58c0.netlify.app"
 ]
